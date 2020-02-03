b3f2e8b003aed0e031550167aff1f1bc847e91ac
hp-edu <hhp17@naver.com>
2020-02-02 23:14:19 -0500

    GetCards Scan method

--- lambda/getCards.js
+++ lambda/getCards.js
@@ -1,9 +1,28 @@
-exports.handler = async (event) => {
-    console.log("Received: "+JSON.stringify(event,null,2));
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+var AWS = require("aws-sdk");
+var documentClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10"
+});
+const tableName = "Cards";
+
+exports.handler = async event => {
+  console.log("Received: " + JSON.stringify(event, null, 2));
+  let response = "";
+  try {
+    var params = {
+      TableName: tableName
     };
-    return response;
+    const cards = await documentClient.scan(params).promise();
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(cards)
+    };
+  } catch (exception) {
+    console.error(exception);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({ "Message: ": exception })
+    };
+  }
+  return response;
 };
