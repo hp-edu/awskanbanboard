4a04af17d0fa1f622eb93ebab923372ce06779ff
hp-edu <hhp17@naver.com>
2020-02-08 02:49:55 -0500

    Complete Put Card

---
+++ lambda/putCard.js
@@ -0,0 +1,44 @@
+var AWS = require("aws-sdk");
+var documentClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10"
+});
+const tableName = "Cards";
+
+exports.handler = async event => {
+  console.log("Received: " + JSON.stringify(event, null, 2));
+  let response = "";
+  try {
+    const id = event.pathParameters.id;
+    const body = JSON.parse(event.body);
+    var params = {
+      TableName: tableName,
+      Item: {
+        id: id,
+        title: body.title,
+        category: body.category
+      }
+    };
+    var params = {
+      TableName: tableName,
+      Key: { id: id },
+      UpdateExpression: "set #c = :c, #t = :t",
+      ExpressionAttributeNames: { "#c": "category", "#t": "title" },
+      ExpressionAttributeValues: {
+        ":c": body.category,
+        ":t": body.title
+      }
+    };
+    await documentClient.update(params).promise();
+
+    response = {
+      statusCode: 200
+    };
+  } catch (exception) {
+    console.error(exception);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({ "Message: ": exception })
+    };
+  }
+  return response;
+};
