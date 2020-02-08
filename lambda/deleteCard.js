aafb643209116d6fce1cb9da1a5c7376e87831ff
hp-edu <hhp17@naver.com>
2020-02-08 04:49:04 -0500

    Complete Delete

---
+++ lambda/deleteCard.js
@@ -0,0 +1,31 @@
+var AWS = require("aws-sdk");
+var documentClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10"
+});
+const tableName = "Cards";
+
+exports.handler = async event => {
+  console.log("Received: " + JSON.stringify(event, null, 2));
+  let response = "";
+  try {
+    const id = event.pathParameters.id;
+    var params = {
+      TableName: tableName,
+      Key: {
+        id: id
+      }
+    };
+    await documentClient.delete(params).promise();
+
+    response = {
+      statusCode: 200
+    };
+  } catch (exception) {
+    console.error(exception);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({ "Message: ": exception })
+    };
+  }
+  return response;
+};
