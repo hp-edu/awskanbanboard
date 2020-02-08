24b4007d006e550f61442fc554483b7b3643c3df
hp-edu <hhp17@naver.com>
2020-02-08 00:10:15 -0500

    POST Card

---
+++ lambda/postCard.js
@@ -0,0 +1,35 @@
+var AWS = require("aws-sdk");
+var documentClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10"
+});
+const tableName = "Cards";
+
+exports.handler = async event => {
+  console.log("Received: " + JSON.stringify(event, null, 2));
+  let response = "";
+  try {
+    const id = event.requestContext.requestId;
+    const body = JSON.parse(event.body);
+    var params = {
+      TableName: tableName,
+      Item: {
+        id: id,
+        title: body.title,
+        category: body.category
+      }
+    };
+    await documentClient.put(params).promise();
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({ id: id })
+    };
+  } catch (exception) {
+    console.error(exception);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({ "Message: ": exception })
+    };
+  }
+  return response;
+};
