2e66d8c449024ebee72b242b991b6e9e9c297cbd
Hyukpyo Hong <hyukpyohong@gmail.com>
2020-02-29 00:23:56 -0500

    Add XRay SDK

--- lambda/deleteCard.js
+++ lambda/deleteCard.js
@@ -1,15 +1,20 @@
-var AWS = require("aws-sdk");
+var AWSXRay = require("aws-xray-sdk");
+var AWS = AWSXRay.captureAWS(require("aws-sdk"));
 var documentClient = new AWS.DynamoDB.DocumentClient({
   apiVersion: "2012-08-10"
 });
 const tableName = "Cards";
 
 exports.handler = async event => {
+  var segment = AWSXRay.getSegment();
+  var subsegment = segment.addNewSubsegment("Main Logic");
+  subsegment.addAnnotation("App", "Kanban Lambda");
   console.log("Received: " + JSON.stringify(event, null, 2));
   let response = "";
+  var params;
   try {
     const id = event.pathParameters.id;
-    var params = {
+    params = {
       TableName: tableName,
       Key: {
         id: id
@@ -30,8 +35,13 @@ exports.handler = async event => {
       headers: {
         "Access-Control-Allow-Origin": "*"
       },
-      body: JSON.stringify({ "Message: ": exception })
+      body: JSON.stringify({ "Message: ": "서버 에러" })
     };
+    subsegment.addMetadata("Exception", exception.stack.toString());
+    subsegment.addMetadata("Event", event);
+    subsegment.addMetadata("Parameter", params);
+    subsegment.close(exception);
   }
+  subsegment.close();
   return response;
 };
--- lambda/postCard.js
+++ lambda/postCard.js
@@ -1,16 +1,21 @@
-var AWS = require("aws-sdk");
+var AWSXRay = require("aws-xray-sdk");
+var AWS = AWSXRay.captureAWS(require("aws-sdk"));
 var documentClient = new AWS.DynamoDB.DocumentClient({
   apiVersion: "2012-08-10"
 });
 const tableName = "Cards";
 
 exports.handler = async event => {
+  var segment = AWSXRay.getSegment();
+  var subsegment = segment.addNewSubsegment("Main Logic");
+  subsegment.addAnnotation("App", "Kanban Lambda");
   console.log("Received: " + JSON.stringify(event, null, 2));
   let response = "";
+  var params;
   try {
     const id = event.requestContext.requestId;
     const body = JSON.parse(event.body);
-    var params = {
+    params = {
       TableName: tableName,
       Item: {
         id: id,
@@ -34,8 +39,13 @@ exports.handler = async event => {
       headers: {
         "Access-Control-Allow-Origin": "*"
       },
-      body: JSON.stringify({ "Message: ": exception })
+      body: JSON.stringify({ "Message: ": "서버 에러" })
     };
+    subsegment.addMetadata("Exception", exception.stack.toString());
+    subsegment.addMetadata("Event", event);
+    subsegment.addMetadata("Parameter", params);
+    subsegment.close(exception);
   }
+  subsegment.close();
   return response;
 };
